975339f9a5586af2994493a975c8752920635c08
Krunoslav Husak <kruno@husak.me>
2023-04-22 22:31:39 +0200

    Initial support for API with Fiber

---
+++ api/api.go
@@ -0,0 +1,53 @@
+package api
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/h00s/bitbox/api/config"
+)
+
+type API struct {
+	config *config.Config
+	server *fiber.App
+	//services *services.Services
+}
+
+func NewAPI(config *config.Config, logger *log.Logger) *API {
+	server := fiber.New()
+	//services := services.NewServices(database, memStore, logger)
+	//servicesMiddleware := middleware.NewServicesMiddleware(services)
+	//modelsMiddleware := middleware.NewModelsMiddleware(services)
+	//limiterMiddleware := middleware.NewLimiterMiddleware(&config.Limiter)
+
+	//server.Use(servicesMiddleware.ServicesMiddleware)
+	//server.Use(modelsMiddleware.ModelsMiddleware)
+	//server.Use(limiterMiddleware.LimiterMiddleware())
+
+	return &API{
+		config: config,
+		server: server,
+	}
+}
+
+func (api *API) Start() {
+	//api.services.Logger.Println("Starting server on :8080")
+	api.setRoutes()
+	go func() {
+		if err := api.server.Listen(":8080"); err != nil && err != http.ErrServerClosed {
+			//api.services.Logger.Fatal("Error starting server: " + err.Error())
+		}
+	}()
+}
+
+func (api *API) WaitForShutdown() {
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	if err := api.server.Shutdown(); err != nil {
+		//api.services.Logger.Fatal(err)
+	}
+}
--- main.go
+++ main.go
@@ -4,11 +4,15 @@ import (
 	"log"
 	"os"
 
+	"github.com/h00s/bitbox/api"
 	"github.com/h00s/bitbox/api/config"
 )
 
 func main() {
 	config := config.NewConfig()
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
-	logger.Println(config.Database.Host)
+
+	api := api.NewAPI(config, logger)
+	api.Start()
+	api.WaitForShutdown()
 }
