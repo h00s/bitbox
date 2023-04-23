6e45f7e0a4df7d98012d2c81cbb6c56dc62dbca8
Krunoslav Husak <kruno@husak.me>
2023-04-24 00:50:01 +0200

    [APP] Navbar style

--- app/src/navbar/Navbar.svelte
+++ app/src/navbar/Navbar.svelte
@@ -1,4 +1,11 @@
-<nav class="flex items-center flex-wrap bg-gray-800 p-4">
+<style>
+  .navbar {
+    background-color: #282C34;
+    border-bottom: 1px solid #696b71;
+  }
+</style>
+
+<nav class="flex items-center flex-wrap navbar p-4">
   <div class="flex items-center flex-shrink-0 text-white mr-6">
     <span class="font-semibold text-xl tracking-tight">BitBox</span>
   </div>
