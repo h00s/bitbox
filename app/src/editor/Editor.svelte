8f0b32b40aff6f2baa20a89a3ae11186ab438472
Krunoslav Husak <kruno@husak.me>
2023-04-23 11:20:40 +0200

    [APP] Added Ace editor

--- app/src/editor/Editor.svelte
+++ app/src/editor/Editor.svelte
@@ -1,3 +1,20 @@
-<div class="bg-gray-600 flex justify-center items-center flex-grow">
-  editor here
+<script>
+  import { onMount } from "svelte";
+  import ace from "ace-builds/src-noconflict/ace";
+  import "ace-builds/src-noconflict/theme-one_dark";
+  import "ace-builds/src-noconflict/mode-javascript";
+
+  let editor;
+
+  onMount(() => {
+    editor = ace.edit("ace-editor");
+    editor.setTheme("ace/theme/one_dark");
+    editor.getSession().setMode("ace/mode/javascript");
+    editor.setOptions({
+      showPrintMargin: false,
+    });
+  });
+</script>
+
+<div id="ace-editor" class="flex justify-center items-center flex-grow">
 </div>
