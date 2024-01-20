8856d7ee3f23a9ecbd54f15cd4b40410a0905c5a
Krunoslav Husak <kruno@husak.me>
2024-01-20 10:34:31 +0100

    Updated Raptor to v0.3.2

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"github.com/go-raptor/raptor"
 	"github.com/h00s/bitbox/config"
 	"github.com/h00s/bitbox/config/initializers"
-	"github.com/h00s/raptor"
 )
 
 func main() {
