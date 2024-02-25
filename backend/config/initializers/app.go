479ce96fdb65980651f11e3ca1d042eefc857dfc
Krunoslav Husak <kruno@husak.me>
2024-02-26 00:02:12 +0100

    Updated project structure to Raptor v0.6.4

--- backend/config/initializers/app.go
+++ backend/config/initializers/app.go
@@ -2,17 +2,13 @@ package initializers
 
 import (
 	"github.com/go-raptor/raptor"
-	"github.com/h00s/bitbox/app/controllers"
 )
 
 func App() *raptor.AppInitializer {
 	return &raptor.AppInitializer{
-		Services: raptor.Services{},
-
-		Middlewares: raptor.Middlewares{},
-
-		Controllers: raptor.Controllers{
-			&controllers.SPAController{},
-		},
+		Database:    Database(),
+		Services:    Services(),
+		Middlewares: Middlewares(),
+		Controllers: Controllers(),
 	}
 }
