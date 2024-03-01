369b9a698c6288409ffe0a73d932f3de4b633283
Krunoslav Husak <kruno@husak.me>
2024-03-01 13:12:06 +0100

    Added shadow to title to match icon styling

--- frontend/src/lib/components/Navbar.svelte
+++ frontend/src/lib/components/Navbar.svelte
@@ -1,7 +1,7 @@
 <nav class="flex items-center flex-wrap navbar">
   <div class="flex items-center flex-shrink-0 text-white mr-6">
     <img src="/favicon.png" alt="BitBox" class="ml-1 w-6 h-6" />
-    <span class="font-semibold text-xl tracking-tight pl-2 pr-2">BitBox</span>
+    <span class="title font-semibold text-xl tracking-tight pl-2 pr-2">BitBox</span>
   </div>
 </nav>
 
@@ -10,5 +10,8 @@
     background-color: #34425A;
     border-bottom: 1px solid #64728C;
     min-height: 3rem;
+  }
+  .title {
+    text-shadow: #222222 0px 2px 4px;
   }
 </style>
