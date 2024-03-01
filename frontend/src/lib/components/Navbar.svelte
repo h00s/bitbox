456001968aa92bc7498b40d885fbd0838b86ec05
Krunoslav Husak <kruno@husak.me>
2024-03-01 10:55:56 +0100

    Navbar is now smaller and has an icon

--- frontend/src/lib/components/Navbar.svelte
+++ frontend/src/lib/components/Navbar.svelte
@@ -1,6 +1,7 @@
-<nav class="flex items-center flex-wrap navbar p-4">
+<nav class="flex items-center flex-wrap navbar">
   <div class="flex items-center flex-shrink-0 text-white mr-6">
-    <span class="font-semibold text-xl tracking-tight">BitBox</span>
+    <img src="/favicon.png" alt="BitBox" class="ml-1 w-6 h-6" />
+    <span class="font-semibold text-xl tracking-tight pl-2 pr-2">BitBox</span>
   </div>
 </nav>
 
@@ -8,5 +9,6 @@
   .navbar {
     background-color: #34425A;
     border-bottom: 1px solid #64728C;
+    min-height: 3rem;
   }
 </style>
