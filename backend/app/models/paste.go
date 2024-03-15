67d591b802e371c5ac90e2b21f33b14fe87aaaa2
Krunoslav Husak <kruno@husak.me>
2024-03-15 23:53:44 +0100

    Added PublicPaste

--- backend/app/models/paste.go
+++ backend/app/models/paste.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"time"
+)
+
+const validChars = "bcdfghmnprstvz23456789"
 
 type Paste struct {
 	ID        uint      `gorm:"primaryKey" json:"id"`
@@ -10,4 +14,34 @@ type Paste struct {
 	UpdatedAt time.Time `json:"updated_at"`
 }
 
+type PublicPaste struct {
+	ID string
+	Paste
+}
+
+func (p *Paste) ToPublicPaste() PublicPaste {
+	return PublicPaste{
+		ID:    shortURIfromID(p.ID),
+		Paste: *p,
+	}
+}
+
 type Pastes []Paste
+type PublicPastes []PublicPaste
+
+func shortURIfromID(id uint) string {
+	uri := ""
+	for id > 0 {
+		uri = string(validChars[id%uint(len(validChars))]) + uri
+		id = id / uint(len(validChars))
+	}
+	return uri
+}
+
+/*func idFromShortURI(uri string) uint {
+	id := uint(0)
+	for _, c := range uri {
+		id = id*uint(len(validChars)) + uint(strings.Index(validChars, string(c)))
+	}
+	return id
+}*/
